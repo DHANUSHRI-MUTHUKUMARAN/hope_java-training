e8288babc3180bc0fa269deeb7272cd172e8a8bc
thananauto <thananjayan1988@gmail.com>
2026-04-08 08:28:19 +0530

    DAY 13

---
+++ day 13/LC_121.java
@@ -0,0 +1,12 @@
+public class LC_121 {
+    public int maxProfit(int[] prices) {
+        int min = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price: prices) {
+            min = Math.min(price, min);
+            maxProfit = Math.max(maxProfit, price - min);
+        }
+        return maxProfit;
+    }
+}
